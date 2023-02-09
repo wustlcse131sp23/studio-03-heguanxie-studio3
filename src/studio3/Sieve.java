f705b976396d5cdbd77f6bf2d8a60df072ff19c2
hannah <hannah@hannahdembp-2.dhcp.wustl.edu>
2023-02-09 12:56:36 -0600

    studio3

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,30 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("n = ?");
+		int n = in.nextInt();
+		boolean [] primeNumbers = new boolean[n];
+		for (int i = 0; i < n; i ++)
+		{
+			primeNumbers [i] = true;
+		}
+		for (int factor = 2; factor < n ; factor++)
+		{
+			for (int multiple = factor + factor; multiple < n; multiple+=factor )
+			{
+				primeNumbers[multiple] = false;	
+			}
+		}
+		for (int i = 1; i < n; i++)
+		{
+			System.out.println(primeNumbers[i]);
+		}
+	}
+
+}
